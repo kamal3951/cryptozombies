a20105b4ed2e6f4a584fc968fd21eb0283b8195a
Kamal <kamalproacc@gmail.com>
2022-03-05 23:08:59 +0530

    added the smart contracts

---
+++ smartcontracts-hardhat/contracts/zombiefactory.sol
@@ -0,0 +1,66 @@
+pragma solidity >=0.8.0 <0.9.0;
+
+import "./ownable.sol";
+import "./safemath.sol";
+
+contract ZombieFactory is Ownable { //contract ZombieFactory which is inherited from contract Ownable
+
+  using SafeMath for uint256; // using SafeMath library for uint256, stops over/under flow of uint256
+  using SafeMath32 for uint32; 
+  using SafeMath16 for uint16;
+
+  event NewZombie(uint zombieId, string name, uint dna); //event used to interect with the front end
+
+  uint dnaDigits = 16;                //will be used to calculate dnaModulus
+  uint dnaModulus = 10 ** dnaDigits;  //dnaModulus will be used to mod a uint to get a 16 digit
+                                      //uint which will act as the dna of the zombie
+                                      //since, x%10 gives single digit number
+                                      //       x%100 gives double digit number
+                                      //       x&10^n gives n digit number
+
+uint cooldownTime = 1 days;           //time between two concesutive attacks by an zombie
+
+  struct Zombie {                     //struct decleration for a zombie
+    string name;
+    uint dna;
+    uint32 level;
+    uint32 readyTime;
+    uint16 winCount;
+    uint16 lossCount;
+  }
+
+  Zombie[] public zombies;            //array of zombie named zombies
+
+  mapping (uint => address) public zombieToOwner; //mapping from zombieId to owner address
+  mapping (address => uint) ownerZombieCount;     //mapping from owner address to its zombie count
+
+  function _createZombie(string memory _name, uint _dna) internal {  /*fn to create zombie with
+  desired name and dna. This function is reserved to be called only internally since we don't
+  want users to create a zombie of any name and data, it will be called by other fn which will
+  assign a dna and name from its side to the zombie*/
+
+    uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
+    /*this line pushes the zombie in the zombies array and returns the lenght of the array
+    we -1 from the length and stors it in the id uint which gives us the id of the zombie*/
+
+    zombieToOwner[id] = msg.sender;  //msg.sender which will be the account or contract calling
+    //this function is set to be the owner of the zombie with ID id.
+    ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].add(1);
+    emit NewZombie(id, _name, _dna);
+  }
+
+  function _generateRandomDna(string memory _str) private view returns (uint){
+    //this fn generates a randon dna from a string provided
+    uint rand = uint(keccak256(abi.encodePacked(_str))); //uint256 is generated
+    return rand % dnaModulus;       // mod with dnaModulus to generate a 16 digit number
+  }
+
+  function createRandomZombie(string memory _name) public{
+    //this fn uses the _generateRandomDna to generate a dna with name as provided
+    require(ownerZombieCount[msg.sender] == 0); //the owner shoud not already have a zombie
+    uint randDna = _generateRandomDna(_name);   //randDna is generated
+    randDna = randDna - randDna % 100;          //the last two digits are set to 00
+    _createZombie(_name, randDna);
+  }
+
+}
---
+++ smartcontracts-hardhat/contracts/zombiefeeding.sol
@@ -0,0 +1,60 @@
+//SPDX-License-Identifier:MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "./zombiefactory.sol";      //zombiefactory is imported
+ 
+contract KittyInterface {  //interface KittyInterface is defined to interact with the kitty
+                           //contract which is stored somewhere in the blockchain 
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+
+contract ZombieFeeding is ZombieFactory {  //inheritence
+
+  KittyInterface kittyContract;           //KittyInterface's object kittyContract is defined
+
+  modifier onlyOwnerOf(uint _zombieId) {  //moodifier is a fn used with other fns to add a recurring conditon
+    require(msg.sender == zombieToOwner[_zombieId]);  //require statement
+    _;                                                //idk whats that
+  }
+
+  function setKittyContractAddress(address _address) external onlyOwner {
+    kittyContract = KittyInterface(_address);
+  }
+
+  function _triggerCooldown(Zombie storage _zombie) internal {
+    _zombie.readyTime = uint32(now + cooldownTime);
+  }
+
+  function _isReady(Zombie storage _zombie) internal view returns (bool) {
+      return (_zombie.readyTime <= now);
+  }
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal onlyOwnerOf(_zombieId) {
+    Zombie storage myZombie = zombies[_zombieId];
+    require(_isReady(myZombie));
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+      newDna = newDna - newDna % 100 + 99;
+    }
+    _createZombie("NoName", newDna);
+    _triggerCooldown(myZombie);
+  }
+
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
+  }
+}
